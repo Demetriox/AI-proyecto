5779b7e80b107e7ad5ed339a1340b021692361e4
Demetriox <jesusdemetrio04@gmail.com>
2019-12-02 19:24:51 -0700

    naruto try

--- sketch.js
+++ sketch.js
@@ -1,56 +1,135 @@
 let mobilenet;
-let predictor;
+let classifier;
 let video;
-let value = 0;
-let slider;
-let addButton;
+let rataButton, serpienteButton, jabaliButton, dragonButton, monoButton, bueyButton, caballoButton, ovejaButton;
+let tigreButton, pajaroButton, perroButton, conejoButton;
+let label = 'loading model';
 let trainButton;
-var images = [];
+var labeleado;
 
-//INPUT IMAGES
-(function () {
-	var fileCatcher = document.getElementById('file-catcher');
-  var fileInput = document.getElementById('file-input');
-  var fileListDisplay = document.getElementById('file-list-display');
-  
-  var fileList = [];
-  var renderFileList, sendFile;
-  
-  fileCatcher.addEventListener('submit', function (evnt) {
-  	evnt.preventDefault();
-    fileList.forEach(function (file) {
-      console.log(file);
-      predictor.addExample(file, slider.value());
-    });
-  });
-  
-  fileInput.addEventListener('change', function (evnt) {
- 		fileList = [];
-  	for (var i = 0; i < fileInput.files.length; i++) {
-    	fileList.push(fileInput.files[i]);
-    }
-    renderFileList();
-  });
-  
-
-})();
-
-//END INPUT IMG
 
 function modelReady() {
   console.log('Model is ready!!!');
 }
 
+
 function videoReady() {
   console.log('Video is ready!!!');
 }
 
+function setup() {
+  createCanvas(320, 270);
+  video = createCapture(VIDEO);
+  video.hide();
+  background(0);
+  mobilenet = ml5.featureExtractor('MobileNet', modelReady);
+  classifier = mobilenet.classification(video, videoReady);
+  rataButton = createButton('rata');
+  rataButton.mousePressed(function () {
+    classifier.addImage('rata');
+  });
+  serpienteButton = createButton('serpiente');
+  serpienteButton.mousePressed(function () {
+    classifier.addImage('serpiente');
+  });
+  bueyButton = createButton('buey');
+  bueyButton.mousePressed(function () {
+    classifier.addImage('buey');
+  });
+  jabaliButton = createButton('jabali');
+  jabaliButton.mousePressed(function () {
+    classifier.addImage('jabali');
+  });
+  dragonButton = createButton('dragon');
+  dragonButton.mousePressed(function () {
+    classifier.addImage('dragon');
+  });
+  conejoButton = createButton('conejo');
+  conejoButton.mousePressed(function () {
+    classifier.addImage('conejo');
+  });
+  pajaroButton = createButton('pajaro');
+  pajaroButton.mousePressed(function () {
+    classifier.addImage('pajaro');
+  });
+  caballoButton = createButton('caballo');
+  caballoButton.mousePressed(function () {
+    classifier.addImage('caballo');
+  });
+  tigreButton = createButton('tigre');
+  tigreButton.mousePressed(function () {
+    classifier.addImage('tigre');
+  });
+  ovejaButton = createButton('oveja');
+  ovejaButton.mousePressed(function () {
+    classifier.addImage('ovejea');
+  });
+  liebreButton = createButton('liebre');
+  liebreButton.mousePressed(function () {
+    classifier.addImage('liebre');
+  });
 
-// Entrenar el modelo
+  monoButton = createButton('mono');
+  monoButton.mousePressed(function () {
+    classifier.addImage('mono');
+  });
+
+  perroButton = createButton('perro');
+  perroButton.mousePressed(function () {
+    classifier.addImage('perro');
+  });
+
+
+  trex = createButton('nnn');
+  
+  
+  trex = createButton('nnn');
+
+  trex = createButton('nnn');
+
+  trex = createButton('nnn');
+  trex = createButton('nnn');
+
+  trex = createButton('nnn');
+
+  trex = createButton('nnn');
+
+
+
+  trainButton = createButton('train');
+  trainButton.mousePressed(function () {
+    classifier.train(whileTraining);
+  });
+
+  saveButton = createButton('save');
+  saveButton.mousePressed(function () {
+    classifier.save();
+  });
+}
+
+function draw() {
+  background(0);
+  image(video, 0, 0, 320, 240);
+  fill(255);
+  textSize(16);
+  text(label[0].label, 10, height - 10);
+  labeleado = label[0].label;
+  /*
+  if(labeleado == "rata"){
+    var img = document.createElement("img");
+    img.src = "images/rata.JPG";
+    var src = document.getElementById("header");
+    src.appendChild(img);
+  }
+*/
+ 
+  }
+
+
 function whileTraining(loss) {
   if (loss == null) {
     console.log('Training Complete');
-    predictor.predict(gotResults);
+    classifier.classify(gotResults);
   } else {
     console.log(loss);
   }
@@ -61,49 +140,7 @@ function gotResults(error, result) {
   if (error) {
     console.error(error);
   } else {
-    value = result;
-    predictor.predict(gotResults);
+    label = result;
+    classifier.classify(gotResults);
   }
-}
-
-
-
-//BOTTONES PARA ENTRENAR MODELO
-function setup() {
-  createCanvas(320, 270);
-  video = createCapture(VIDEO);
-  video.hide();
-  background(0);
-  mobilenet = ml5.featureExtractor('MobileNet', modelReady);
-  predictor = mobilenet.regression(video, videoReady);
-  //addOtherImg = ml5.featureExtractor.addImage(input, label, imageAdded);
-
-  slider = createSlider(0, 1, 0.5, 0.01);
-  addButton = createButton('add example image');
-  addButton.mousePressed(function() {
-    predictor.addImage(slider.value());
-  });
-  trainButton = createButton('train');
-  trainButton.mousePressed(function() {
-    predictor.train(whileTraining);
-  });
-  saveButton = createButton('save');
-  saveButton.mousePressed(function () {
-    predictor.save();
-  });
-
-}
-
-
-// DIBUJAR CANVAS Y RECTANGULO DE VALORES.
-function draw() {
-  background(0);
-  image(video, 0, 0, 320, 240);
-  rectMode(CENTER);
-  fill(255, 0, 200);
-  console.log(value);
-  rect(value.value * width, height / 2, 50, 50);
-  fill(255);
-  textSize(16);
-  text(value.value , 10, height - 10);
 }
