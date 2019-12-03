ac576dc1b64724835d38e71dfcb29b6358e4855d
Demetriox <jesusdemetrio04@gmail.com>
2019-12-02 20:17:41 -0700

    images

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let tigreButton, pajaroButton, perroButton, conejoButton;
 let label = 'loading model';
 let trainButton;
 var labeleado;
-
+var src = document.getElementById("header");
 
 function modelReady() {
   console.log('Model is ready!!!');
@@ -114,14 +114,79 @@ function draw() {
   textSize(16);
   text(label[0].label, 10, height - 10);
   labeleado = label[0].label;
-  /*
+  
   if(labeleado == "rata"){
+    src.innerHTML = "";
     var img = document.createElement("img");
     img.src = "images/rata.JPG";
-    var src = document.getElementById("header");
+    src.appendChild(img);
+  } else if (labeleado == "buey"){
+    src.innerHTML = "";
+    var img = document.createElement("img");
+    img.src = "images/buey.JPG";
     src.appendChild(img);
   }
-*/
+  else if (labeleado == "caballo"){
+    src.innerHTML = "";
+    var img = document.createElement("img");
+    img.src = "images/caballo.JPG";
+    src.appendChild(img);
+  }
+  else if (labeleado == "dragon"){
+    src.innerHTML = "";
+    var img = document.createElement("img");
+    img.src = "images/dragon.JPG";
+    src.appendChild(img);
+  }
+  else if (labeleado == "jabali"){
+    src.innerHTML = "";
+    var img = document.createElement("img");
+    img.src = "images/jabali.JPG";
+    src.appendChild(img);
+  }
+  else if (labeleado == "liebre"){
+    src.innerHTML = "";
+    var img = document.createElement("img");
+    img.src = "images/liebre.JPG";
+    src.appendChild(img);
+  }
+  else if (labeleado == "mono"){
+    src.innerHTML = "";
+    var img = document.createElement("img");
+    img.src = "images/mono.JPG";
+    src.appendChild(img);
+  }
+  else if (labeleado == "oveja"){
+    src.innerHTML = "";
+    var img = document.createElement("img");
+    img.src = "images/oveja.JPG";
+    src.appendChild(img);
+  }
+  else if (labeleado == "pajaro"){
+    src.innerHTML = "";
+    var img = document.createElement("img");
+    img.src = "images/pajaro.JPG";
+    src.appendChild(img);
+  }
+  else if (labeleado == "perro"){
+    src.innerHTML = "";
+    var img = document.createElement("img");
+    img.src = "images/perro.JPG";
+    src.appendChild(img);
+  }
+  else if (labeleado == "serpiente"){
+    src.innerHTML = "";
+    var img = document.createElement("img");
+    img.src = "images/serpiente.JPG";
+    src.appendChild(img);
+  }
+  else if (labeleado == "tigre"){
+    src.innerHTML = "";
+    var img = document.createElement("img");
+    img.src = "images/tigre.JPG";
+    src.appendChild(img);
+  }
+
  
   }
 
