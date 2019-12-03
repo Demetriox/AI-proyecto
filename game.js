6b10c52422b72f2168f821070cdd5700a4ce5099
Eduardo Flores <edfloreshz@gmail.com>
2019-12-03 00:57:44 -0700

    Test 1
    Co-authored-by: Demetriox <jesusdemetrio04@gmail.com>

--- game.js
+++ game.js
@@ -1,21 +1,4 @@
-var posiciones = [
-    { key: "pajaro", value: "images/pajaro.JPG" },
-    { key: "jabali", value: "images/jabali.JPG" },
-    { key: "perro", value: "images/perro.JPG" },
-    { key: "dragon", value: "images/dragon.JPG" },
-
-    { key: "liebre", value: "images/liebre.JPG" },
-    { key: "caballo", value: "images/caballo.JPG" },
-    { key: "mono", value: "images/mono.JPG" },
-    { key: "buey", value: "images/buey.JPG" },
-
-    { key: "rata", value: "images/rata.JPG" },
-    { key: "serpiente", value: "images/serpiente.JPG" },
-    { key: "tigre", value: "images/tigre.JPG" },
-    { key: "carnero", value: "images/carnero.JPG" },
-];
 var src = document.getElementById("header");
-let i = posiciones.len
 let mobilenet;
 let classifier;
 let video;
@@ -24,24 +7,38 @@ let trainButton;
 var labeleado;
 var contador = 0;
 
+var posiciones = [
+    // { key: "pajaro", value: "images/pajaro.JPG" },
+    // { key: "jabali", value: "images/jabali.JPG" },
+    // { key: "perro", value: "images/perro.JPG" },
+    // { key: "dragon", value: "images/dragon.JPG" },
+
+    // { key: "liebre", value: "images/liebre.JPG" },
+    // { key: "caballo", value: "images/caballo.JPG" },
+    // { key: "mono", value: "images/mono.JPG" },
+    // { key: "buey", value: "images/buey.JPG" },
+
+    { key: "rata", value: "images/rata.JPG" },
+    { key: "serpiente", value: "images/serpiente.JPG" },
+    // { key: "tigre", value: "images/tigre.JPG" },
+    // { key: "carnero", value: "images/carnero.JPG" },
+];
+var i = posiciones.length;
+
 function setup() {
     createCanvas(320, 270);
     video = createCapture(VIDEO);
     video.hide();
     background(0);
-    mobilenet = ml5.featureExtractor('MobileNet');
+    mobilenet = ml5.featureExtractor('MobileNet', console.log("ML5 Loaded"));
     classifier = mobilenet.classification(video);
     classifier.load('model.json', console.log("Model is ready."));
     classifier.classify(gotResults);
-    var random = [];
-    for (var i; i < 5; i++) {
-        random.push(posiciones[(Math.round(Math.random() * 12))]);
-    }
-    posiciones.filter(random.key);
+    posiciones.sort(function(){return 0.5 - Math.random()});
     var img = document.createElement("img");
     img.src = posiciones[i - 1].value;
+    console.log(posiciones[i - 1].value, i)
     src.appendChild(img);
-    // posiciones.sort(function(){return 0.5 - Math.random()});
 }
 
 function draw() {
@@ -50,14 +47,16 @@ function draw() {
     fill(255);
     textSize(16);
     text(label[0].label, 10, height - 10);
-    if (label[0].label == posiciones[i - 1].key) {
-        src.innerHTML = "";
-        var img = document.createElement("img");
-        img.src = posiciones[i - 1].value;
-        src.appendChild(img);
-        posiciones.pop();
-        i--;
-    }
+    classifier.classify(gotResults);
+
+    // if (label[0].label == posiciones[i - 1].key) {
+    //     src.innerHTML = "";
+    //     var img = document.createElement("img");
+    //     img.src = posiciones[i - 1].value;
+    //     src.appendChild(img);
+    //     posiciones.pop();
+    //     i--;
+    // }
 }
 
 function gotResults(error, result) {
